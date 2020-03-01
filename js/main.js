b08997d8b5c67ae164000095dd458540b41f6ef1
Igor Pakshin <38372496+mephistofeel@users.noreply.github.com>
2020-03-01 15:14:02 +0300

    Оживил слайдер в шапке

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
 
+  $('.header-slider').slick({
+    arrows: false,
+    vertical: true,
+    dots: true,
+    dotsClass: 'header-slider__dots',
+    autoplay: true,
+    autoplaySpeed: 4000
+  });
+
 });
