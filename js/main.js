944596ba6806de7d606564a03897313417e60ef6
Igor Pakshin <38372496+mephistofeel@users.noreply.github.com>
2020-03-21 08:34:55 +0300

    Сделал адаптивный слайдер в отзывах

--- js/main.js
+++ js/main.js
@@ -5,13 +5,35 @@ $(function(){
     dots: true,
     dotsClass: 'header-slider__dots',
     vertical: true,
-    // autoplay: true,
-    // autoplaySpeed: 4000
-    // responsive: [
-    //   {
-    //     breakpoint: 
-    //   }
-    // ]
+    autoplay: true,
+    autoplaySpeed: 4000
+  });
+
+  $('.reviews__list').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          autoplaySpeed: 8000
+        }
+      },
+      {
+        breakpoint: 795,
+        settings: {
+          slidesToShow: 1,
+          autoplaySpeed: 6000
+        }
+      }
+    ]  
+    
   });
 
   $('.page-header__toggle').on('click', function(){
